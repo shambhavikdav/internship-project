54f0111af4961bba5ac1bdac19047435ff5cd1a3
shambhavikdav <59994855+shambhavikdav@users.noreply.github.com>
2020-08-02 15:54:14 +0530

    Add files via upload

---
+++ College-Management-System 2/AboutManagement.php
@@ -0,0 +1,27 @@
+<!Doctype>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css"/>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/home.css" />
+        <title>This project is develope by Shambhavi Sayli and Pooja</title>
+    </head>
+    <body>
+        <div class="panel panel-default">
+            <div class="panel-heading"><h1 style="text-align: center;"><span class="glyphicon glyphicon-user"></span> About Project!</h1></div>
+  			<div class="panel-body">
+			<div class="container">
+				<p style="text-align:center;">ssp</p>
+                        </div><br><br>
+                        
+                        <h1><strong>Project title: </strong>ERP System for College Management System</h1>
+                        <h1><strong>Made by: Shambhavi Sayali and Pooja</strong></h1>
+                        <h1><strong>College: </strong>AP SHAH INSTITUTE OF TECHNOLOGY</h1>
+                        <h1><strong>Branch: </strong>Computer science.</h1>
+                        </div>
+        </div>
+    </body>
+</html>
---
+++ College-Management-System 2/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>ERP For College management system made by Shambhavi kulkarni, Sayali jadhav, and Pooja maniyar</title>
+</head>
+
+<body>
+	<div class="panel panel-default">
+  		<div class="panel-heading"><h1>ERP For College Management System</h1></div>
+                <div class="panel-body"><br><br>
+        <div>
+            <img src="images/rtupic.jpg" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
+        </div><br><br>
+        <div class="middle_text">
+            <p><strong>A. P. Shah Institute of Technology (APSIT) has started functioning with commitment of imparting state of art technical education so as to inculcate conceptual know-how, analyzing skills, decision making abilities and leadership qualities in the students. APSIT stands committed to the intellectual and moral growth of every student.
+
+   APSIT has experienced and proficient team which aspires to unlock the hidden potential in subconscious minds of students and to create competent Engineers with vision & social commitment.
+
+</strong></p>
+        </div>
+                
+        <div class="text_para_home">
+            <p>
+Institute Vision:
+APSIT aspires to be a premier institute producing globally competent engineering professionals to contribute towards socio-economic growth of India.
+Institute Mission:
+To provide conducive and collaborative environment to meet contemporary & future Engineering challenges by project based and value-added education with the support of trained faculty.
+</p>         
+        </div>   
+                </div>
+        </div>
+    
+    
+</body>
+</html>
